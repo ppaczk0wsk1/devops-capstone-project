a76a66ea99c4779e01e520512676a6a58b836fc7
Pawel Paczkowski <iampauldeveloper@gmail.com>
2026-04-26 08:39:57 -0400

    Fix routes file

--- service/routes.py
+++ service/routes.py
@@ -57,9 +57,10 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
- ######################################################################
- # LIST ALL ACCOUNTS
- ######################################################################
+
+######################################################################
+# LIST ALL ACCOUNTS
+######################################################################
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -91,6 +92,7 @@ def get_accounts(account_id):
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
 
+
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
@@ -110,7 +112,6 @@ def update_accounts(account_id):
     account.update()
 
     return account.serialize(), status.HTTP_200_OK
-
 
 
 ######################################################################
