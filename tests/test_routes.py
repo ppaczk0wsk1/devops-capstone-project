b9f9247903eac7c9b5c2788252f842c242e4e5c3
Pawel Paczkowski <iampauldeveloper@gmail.com>
2026-04-27 06:21:42 -0400

    Add a fix

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -36,17 +36,11 @@ class TestAccountService(TestCase):
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
         app.logger.setLevel(logging.CRITICAL)
         init_db(app)
+        talisman.force_https = False
 
     @classmethod
     def tearDownClass(cls):
         """Runs once before test suite"""
-
-    @classmethod
-    def setUpClass(cls):
-    """Run once before all tests"""
-    { other lines of code here ... }
-    talisman.force_https = False
-
 
     def setUp(self):
         """Runs before each test"""
@@ -180,3 +174,9 @@ class TestAccountService(TestCase):
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
 
+    def test_cors_security(self):
+        """It should return a CORS header"""
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Check for the CORS header
+        self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
